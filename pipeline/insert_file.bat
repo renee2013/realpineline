504474ac2fe34198c205c441131b83570787166d
renee2013 <jfwu@thoughtworks.com>
2013-06-28 19:38:09 +0800

    worked

--- pipeline/insert_file.bat
+++ pipeline/insert_file.bat
@@ -2,67 +2,41 @@
 @echo off
 
 
-SET write=1
+SET write2=1
 
 (FOR /F "tokens=*" %%A IN (config.xml) DO (
           
-     if "%write%" equ "1" (
+     if "!write2!" equ "1" (
 	ECHO %%A
         
         echo "%%A" | findstr /C:"<useSecurity>" 1>null
         if not errorlevel 1 (
           type role_security.txt
-  	  set write=2
-          echo "%write%" "222222222222222222222222222222222222222222222222222"
+  	  set write2=2
+          
          )  
      )
        
-     if "%write%" equ "3" (
+     if "!write2!" equ "3" (
 	ECHO %%A
      ) 
 
-     if "%write%" equ "2" (
+     if "!write2!" equ "2" (
       echo "%%A" | findstr /C:"<projectNamingStrategy" 1>null
       if not errorlevel 1 (
         ECHO %%A 
-  	set write=3
-        echo "%write%" "33333333333333333333333333333333333333333333333333333"
+  	set write2=3
+        
       )
      )
 
 
  )
-)
-REM >temp.txt
-REM move /y temp.txt config.xml
+)>temp.txt
+move /y temp.txt config.xml
 
 GoTO END_SEC
 
-
-
-
-:insert_txt
-
-   FOR /F "tokens=*" %%A IN (config.xml) DO (
-     ECHO %%A
-     REM call:findIt "<useSecurity>" %%A
-    ) >temp.txt
-move /y temp.txt config.xml
-goto :eof
-
-
-:findIt
-
-echo "%~2" | findstr /C:"%~1" 1>nul
-
-if not errorlevel 1 (
-  type role_security.txt
-  echo "111111111111111111111111111111111111111111111111111111111111111111111"
-)
-
- 
-
-goto :eof
 
 :END_SEC
 
